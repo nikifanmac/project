79f33c297516b3f70ebb012e068613187116981b
Nikita <nikifanmac@gmail.com>
2022-08-08 21:24:39 +0300

    добавили вопросы и записали в массив

--- script.js
+++ script.js
@@ -8,4 +8,14 @@ const personalMovieDB = {
   actors: {},
   genres: [],
   privat: false
-}
+};
+
+const movie1 = prompt('Один из последних просмотреных фильмов?', ''),
+      movie1Reit = prompt('На сколько оцените его?', ''),
+      movie2 = prompt('Один из последних просмотреных фильмов?', ''),
+      movie2Reit = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[movie1] = movie1Reit;
+personalMovieDB.movies[movie2] = movie2Reit;
+
+console.log(personalMovieDB);
